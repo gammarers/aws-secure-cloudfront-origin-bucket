8fe77f56e703bc58df754e879875fafa7a0d3963
yicr <yicr@users.noreply.github.com>
2023-01-30 16:34:05 +0900

    feat!: update cdk version.

--- .projenrc.ts
+++ .projenrc.ts
@@ -6,7 +6,7 @@ const PROJECT_DESCRIPTION = 'AWS CloudFront distribution origin S3 bucket.';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
-  cdkVersion: '2.43.0',
+  cdkVersion: '2.61.0',
   defaultReleaseBranch: 'main',
   projenrcTs: true,
   name: PROJECT_NAME,
