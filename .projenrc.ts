c4e06df7b9f89469c82ae2d2bbc1904cda8db245
yicr <yicr@users.noreply.github.com>
2023-08-25 12:39:35 +0900

    chore(deps): deps, peer deps version set changed

--- .projenrc.ts
+++ .projenrc.ts
@@ -14,10 +14,10 @@ const project = new awscdk.AwsCdkConstructLibrary({
   keywords: ['aws', 'cdk', 'cloudfront', 'cdn', 'web', 's3', 'bucket'],
   npmAccess: javascript.NpmAccess.PUBLIC,
   deps: [
-    '@gammarer/aws-secure-bucket@0.12.*',
+    '@gammarer/aws-secure-bucket@~0.12',
   ],
   peerDeps: [
-    '@gammarer/aws-secure-bucket',
+    '@gammarer/aws-secure-bucket@~0.12',
   ],
   minNodeVersion: '18.0.0',
   workflowNodeVersion: '18.17.1',
