8f4a94ced617358229b91d1262e503d5cc83743d
yicr <yicr@users.noreply.github.com>
2024-04-24 13:42:23 +0900

    feat(deps): update deps my packages

--- .projenrc.ts
+++ .projenrc.ts
@@ -16,10 +16,10 @@ const project = new awscdk.AwsCdkConstructLibrary({
   majorVersion: 1,
   npmAccess: javascript.NpmAccess.PUBLIC,
   deps: [
-    '@gammarer/aws-secure-bucket@~1.1.1',
+    '@gammarers/aws-secure-bucket@~1.3.1',
   ],
   peerDeps: [
-    '@gammarer/aws-secure-bucket@~1.1.1',
+    '@gammarers/aws-secure-bucket@~1.3.1',
   ],
   minNodeVersion: '16.0.0',
   workflowNodeVersion: '20.11.0',
